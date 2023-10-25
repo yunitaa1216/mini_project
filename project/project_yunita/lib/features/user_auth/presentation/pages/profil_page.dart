0d2ff18d11625f4934fff72be321df031a5752c7
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-10-26 05:31:16 +0800

    Update profil_page.dart

--- project/project_yunita/lib/features/user_auth/presentation/pages/profil_page.dart
+++ project/project_yunita/lib/features/user_auth/presentation/pages/profil_page.dart
@@ -30,6 +30,10 @@ class _ProfilePageState extends State<ProfilePage> {
     return randomAvatars[index];
   }
 
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+
   @override
 Widget build(BuildContext context) {
   User? user = _authService.getCurrentUser();
@@ -39,43 +43,175 @@ Widget build(BuildContext context) {
     appBar: AppBar(
       title: Text('Profil'),
     ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text('Email: ${user?.email ?? 'Belum masuk'}'),
-          // Text('UID: ${user?.uid ?? 'Belum masuk'}'),
-          AvatarView(
-  radius: 60, // Ukuran avatar
-  borderWidth: 4, // Lebar border
-  borderColor: Colors.white, // Warna border
-  avatarType: AvatarType.CIRCLE, // Bentuk avatar (CIRCLE atau RECTANGLE)
-  backgroundColor: Colors.grey, // Warna latar belakang
-  imagePath: AssetImage('assets/avatars/1.png').assetName, // Gambar avatar default
-),
-          FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-            future: firestore.collection('users').doc(user?.uid).get(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Terjadi kesalahan: ${snapshot.error}');
-              } else if (snapshot.hasData) {
-                final userData = snapshot.data?.data();
-                return Column(
-                  children: [
-                    Text('Username: ${userData?['username'] ?? 'Tidak ada data'}'),
-                    // Tambahkan informasi profil lainnya di sini
-                  ],
-                );
-              } else {
-                return Text('Tidak ada data pengguna.');
-              }
-            },
+    body: 
+    Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('Email: ${user?.email ?? 'Belum masuk'}'),
+            // Text('UID: ${user?.uid ?? 'Belum masuk'}'),
+            AvatarView(
+              radius: 60, // Ukuran avatar
+              borderWidth: 4, // Lebar border
+              borderColor: Colors.white, // Warna border
+              avatarType: AvatarType.CIRCLE, // Bentuk avatar (CIRCLE atau RECTANGLE)
+              backgroundColor: Colors.grey, // Warna latar belakang
+              imagePath: AssetImage('assets/avatars/1.png').assetName, // Gambar avatar default
+            ),
+            Form(
+  key: _formKey,
+  child: Column(
+    children: [
+      ElevatedButton(
+  onPressed: () {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String newEmail = user?.email ?? '';
+        String newUsername = '';
+        String currentUsername = '';
+        
+        return AlertDialog(
+          title: Text('Update Email and Username'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Current Email'),
+                controller: TextEditingController(text: user?.email ?? ''),
+                enabled: false,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Current Username'),
+                controller: TextEditingController(text: currentUsername), // Ganti currentUsername dengan nilai dari Firestore
+                enabled: false,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'New Email'),
+                onChanged: (value) {
+                  newEmail = value;
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'New Username'),
+                onChanged: (value) {
+                  newUsername = value;
+                },
+              ),
+            ],
           ),
-        ],
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (user != null) {
+                  updateUserData(user.uid, newEmail, newUsername);
+                }
+              },
+              child: Text('Update'),
+            ),
+          ],
+        );
+      },
+    );
+  },
+  child: Text('Update Email and Username'),
+),
+ElevatedButton(
+  onPressed: () {
+    if (user != null) {
+      createUserData(user.uid, emailController.text, usernameController.text);
+    }
+  },
+  child: Text('Create New User Data'),
+),
+ElevatedButton(
+  onPressed: () {
+    if (user != null) {
+      // Hapus data pengguna dari Firestore
+      deleteUserData(user.uid);
+    }
+  },
+  child: Text('Delete User Data'),
+),
+
+    ],
+  ),
+),
+            FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              future: firestore.collection('users').doc(user?.uid).get(),
+              builder: (context, snapshot) {
+                String currentUsername = '';
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Terjadi kesalahan: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  final userData = snapshot.data?.data();
+                  currentUsername = userData?['username'] ?? '';
+                  return Column(
+                    children: [
+                      Text('Username: ${currentUsername}'),
+                      // Tambahkan informasi profil lainnya di sini
+                    ],
+                  );
+                } else {
+                  return Text('Tidak ada data pengguna.');
+                }
+              },
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
+
+  // Function to update user data in Firestore
+  void updateUserData(String? userId, String newEmail, String newUsername) async {
+  // Update data di Firestore
+  await firestore.collection('users').doc(userId).update({
+    'email': newEmail,
+    'username': newUsername,
+  });
+
+  // Update data di Firebase Authentication
+  User? user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    await user.updateEmail(newEmail);
+  }
+}
+
+  void createUserData(String userId, String email, String username) {
+  firestore.collection('users').doc(userId).set({
+    'email': email,
+    'username': username,
+  }).then((_) {
+    // Data pengguna baru berhasil dibuat
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Data pengguna baru berhasil dibuat')),
+    );
+  }).catchError((error) {
+    // Gagal membuat data pengguna baru, tampilkan pesan kesalahan
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Gagal membuat data pengguna baru: $error')),
+    );
+  });
+}
+
+void deleteUserData(String userId) async {
+  // Delete data di Firestore
+  await firestore.collection('users').doc(userId).delete();
+
+  // Delete data di Firebase Authentication
+  User? user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    await user.delete();
+  }
 }
 }
